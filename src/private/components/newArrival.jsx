42b54535664e4afaa02b8de02f00cd0d5025567f
Binu Maka <binumaka9@gmail.com>
2025-07-01 17:25:23 +0545

    newarrival updated

--- src/private/components/newArrival.jsx
+++ src/private/components/newArrival.jsx
@@ -1,10 +1,23 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import OrnamentCard from "./OrnamentCard";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const NewArrivals = () => {
   const [ornaments, setOrnaments] = useState([]);
+  const [startIndex, setStartIndex] = useState(0);
   const [loading, setLoading] = useState(true);
+
+  const itemsPerPage = 4;
+  const endIndex = startIndex + itemsPerPage;
+
+  const handlePrev = () => {
+    setStartIndex(Math.max(startIndex - itemsPerPage, 0));
+  };
+
+  const handleNext = () => {
+    setStartIndex(Math.min(startIndex + itemsPerPage, ornaments.length - itemsPerPage));
+  };
 
   useEffect(() => {
     const fetchOrnaments = async () => {
@@ -12,7 +25,7 @@ const NewArrivals = () => {
         const response = await axios.get(`/api/ornament/section/NewArrival`);
         setOrnaments(response.data);
       } catch (error) {
-        console.error("Error fetching destinations:", error);
+        console.error("Error fetching ornaments:", error);
       } finally {
         setLoading(false);
       }
@@ -21,13 +34,21 @@ const NewArrivals = () => {
     fetchOrnaments();
   }, []);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  if (loading) return <div>Loading...</div>;
 
   return (
-    <div>
-      <OrnamentCard section="NewArrival" ornaments={ornaments} />
+    <div className="flex items-center space-x-4 justify-center">
+      {startIndex > 0 && (
+        <button onClick={handlePrev} className="pagination-button">
+          <ChevronLeft className="w-6 h-6 text-gray-600" />
+        </button>
+      )}
+      <OrnamentCard ornaments={ornaments.slice(startIndex, endIndex)} />
+      {endIndex < ornaments.length && (
+        <button onClick={handleNext} className="pagination-button">
+          <ChevronRight className="w-6 h-6 text-gray-600" />
+        </button>
+      )}
     </div>
   );
 };
